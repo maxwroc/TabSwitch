fa6bf8f25956d1884c23f90481ec4244dc3c67c1
maxwroc <max.chodorowski@gmail.com>
2020-04-13 22:26:23 +0100

    Initial code

---
+++ background.js
@@ -0,0 +1,39 @@
+﻿
+chrome.commands.onCommand.addListener(keyboardCommand);
+chrome.tabs.onActivated.addListener(tabActivated);
+
+
+let browserWindows = {};
+
+function keyboardCommand(cmd) {
+    if (cmd != "PreviousTab") {
+        return;
+    }
+
+    chrome.tabs.query({ active: true }, tabsResult => {
+
+        if (tabsResult.length != 1 || !browserWindows[tabsResult[0].windowId]) {
+            return;
+        }
+
+        const tab = tabsResult[0];
+
+        const previousTabs = browserWindows[tab.windowId];
+        if (previousTabs.length < 2) {
+            return;
+        }
+
+        // newest on the beginning
+        const prevTabId = previousTabs[1];
+
+        chrome.tabs.update(prevTabId, { active: true });
+    });
+}
+
+
+function tabActivated(activeInfo) {
+    const windowId = activeInfo.windowId;
+    const tabs = browserWindows[windowId] = browserWindows[windowId] || [];
+    tabs.unshift(activeInfo.tabId);
+    tabs.length = 3;
+}
